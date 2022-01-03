402f0c2b968b2a8005d076007bbe79089e4d0d9a
ROOOO <ql.wang.gd@gmail.com>
2022-01-03 10:41:03 +0800

    [Feature] Time. For motion blur.

--- camera.h
+++ camera.h
@@ -51,10 +51,27 @@ public:
     vertical = 2.0f * half_height * v;
   }
 
+  camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect, float aperture, float focus_dist, float t0, float t1) {
+    time0 = t0;
+    time1 = t1;
+    lens_radius = aperture / 2;
+    float theta = vfov * M_PI / 180.0f;
+    float half_height = tan(theta / 2) * focus_dist;
+    float half_width = aspect * half_height;
+    w = unit_vector(lookfrom - lookat);
+    u = unit_vector(cross(vup, w));
+    v = cross(w, u);
+    origin = lookfrom;
+    lower_left_corner = origin - half_width * u - half_height * v - focus_dist * w;
+    horizontal = 2.0f * half_width * u;
+    vertical = 2.0f * half_height * v;
+  }
+
   ray get_ray(float s, float t) {
     vec3 rd = lens_radius * random_in_unit_disk();
     vec3 offset = u * rd.x() + v * rd.y();
-    return ray(origin + offset, lower_left_corner + s * horizontal + t * vertical - origin - offset);
+    float time = time0 + (time1 - time0) * drand48();
+    return ray(origin + offset, lower_left_corner + s * horizontal + t * vertical - origin - offset, time);
   }
 
   vec3 lower_left_corner;
@@ -62,5 +79,6 @@ public:
   vec3 vertical;
   vec3 origin;
   vec3 u, v, w;
+  float time0{}, time1{};
   float lens_radius{};
 };
