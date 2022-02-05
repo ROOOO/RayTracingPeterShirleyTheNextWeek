e904441d8132eadcf12e39df9e2c416106716126
ROOOO <ql.wang.gd@gmail.com>
2022-02-05 19:59:53 +0800

    [Feature] Add a class for image texture.

--- texture.h
+++ texture.h
@@ -3,6 +3,9 @@
 #include "vec3.h"
 #include "perlin.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
+
 class texture {
 public:
   virtual vec3 value(float u, float v, const vec3& p) const = 0;
@@ -47,4 +50,35 @@ public:
   }
   perlin noise;
   float scale{};
+};
+
+class image_texture : public texture {
+public:
+  image_texture() {}
+  image_texture(unsigned char* pixels, int A, int B)
+    : data(pixels)
+    , nx(A)
+    , ny(B) {}
+  virtual vec3 value(float u, float v, const vec3& p) const {
+    int i = u * nx;
+    int j = (1.0f - v) * ny;
+    if (i < 0) {
+      i = 0;
+    }
+    if (j < 0) {
+      j = 0;
+    }
+    if (i > nx - 1) {
+      i = nx - 1;
+    }
+    if (j > ny - 1) {
+      j = ny - 1;
+    }
+    float r = int(data[3 * i + 3 * nx * j]) / 255.0f;
+    float g = int(data[3 * i + 3 * nx * j + 1]) / 255.0f;
+    float b = int(data[3 * i + 3 * nx * j + 2]) / 255.0f;
+    return vec3(r, g, b);
+  }
+  unsigned char* data;
+  int nx, ny;
 };
