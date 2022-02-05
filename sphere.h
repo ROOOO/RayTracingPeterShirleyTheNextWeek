e82dfa5a271a6851145412bfcbe1a455038aa626
ROOOO <ql.wang.gd@gmail.com>
2022-02-05 19:59:08 +0800

    [Feature] Add the calculation of uv of spheres.

--- sphere.h
+++ sphere.h
@@ -20,6 +20,12 @@ public:
   material* mat_ptr;
 };
 
+void get_sphere_uv(const vec3& p, float& u, float& v) {
+  float phi = atan2(p.z(), p.x());
+  float theta = asin(p.y());
+  u = 1.0f - (phi + M_PI) / (2.0f * M_PI);
+  v = (theta + M_PI * 0.5f) / M_PI;
+}
 bool sphere::hit(const ray& r, float tmin, float tmax, hit_record& rec) const {
   vec3 oc = r.origin() - center;
   float a = dot(r.direction(), r.direction());
@@ -36,6 +42,7 @@ bool sphere::hit(const ray& r, float tmin, float tmax, hit_record& rec) const {
       rec.p = r.point_at_parameter(temp);
       rec.normal = (rec.p - center) / radius;
       rec.mat_ptr = mat_ptr;
+      get_sphere_uv(rec.normal, rec.u, rec.v);
       return true;
     }
   }
